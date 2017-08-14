bcd78ff16a65323b51680c765cae92a8864b5b48
Korsari <euskal.korsari@gmail.com>
2017-08-14 10:30:07 +0100

    test commit

--- TeamsPricing/Program.cs
+++ TeamsPricing/Program.cs
@@ -10,6 +10,8 @@ namespace TeamsPricing
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Not implemented");
+            Console.ReadLine();
         }
     }
 }
